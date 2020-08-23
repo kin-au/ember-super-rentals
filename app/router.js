8852a1e2d9bcbe753d043fc4e8db830a7b910e9c
kin-au <kinnamau@gmail.com>
2020-08-23 19:13:02 +0100

    add routes and links for new pages

--- app/router.js
+++ app/router.js
@@ -1,10 +1,14 @@
-import EmberRouter from '@ember/routing/router';
-import config from 'ember-super-rentals/config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "ember-super-rentals/config/environment";
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function() {
+Router.map(function () {
+  // add a route called "about" which is by default served at url /about
+  this.route("about");
+  // can change the url by doing the below
+  this.route("contact", { path: "/getting-in-touch" });
 });
