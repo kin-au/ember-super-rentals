3336ce58402961c741ffaae4c7eb7b5fbce8ca49
kin-au <kinnamau@gmail.com>
2020-09-04 12:58:30 +0100

    add test for jumbo component

--- tests/integration/components/jumbo-test.js
+++ tests/integration/components/jumbo-test.js
@@ -1,28 +1,21 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import { hbs } from 'ember-cli-htmlbars';
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { render } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
 
 // component test (aka. rendering test) to render/test one component at a time
 
-module('Integration | Component | jumbo', function(hooks) {
+module("Integration | Component | jumbo", function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders content inside jumbo header with tomster', async function(assert) {
+  test("it renders content inside jumbo header with tomster", async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Jumbo>Hello world</Jumbo>`);
+    await render(hbs`<Jumbo>Hello World</Jumbo>`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <Jumbo>
-        template block text
-      </Jumbo>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(".jumbo").exists();
+    assert.dom(".jumbo").hasText("Hello World");
+    assert.dom(".jumbo .tomster").exists();
   });
 });
